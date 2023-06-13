68245d87f40ede9da6fcec819a0a2da1d1facdfa
ArdonitSaliji <ardonit.1980@gmail.com>
2023-06-13 16:35:02 +0200

    location component

--- src/app/pages/weather-location/weather-location.component.ts
+++ src/app/pages/weather-location/weather-location.component.ts
@@ -1,10 +1,49 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
+import { GetLocationService } from 'src/app/services/get-location.service';
 
 @Component({
   selector: 'app-weather-location',
   templateUrl: './weather-location.component.html',
-  styleUrls: ['./weather-location.component.scss']
+  styleUrls: ['./weather-location.component.scss'],
 })
 export class WeatherLocationComponent {
+  constructor(
+    private geoLocation: GetLocationService,
+    private datePipe: DatePipe
+  ) {}
+  userLocationWeather: any = [];
 
+  ngOnInit(): void {
+    // if (this.geoLocation.weatherInformation.length > 0) {
+    //   this.geoLocation.getLocation(this.weatherApi.getUserWeather);
+    // }
+    this.geoLocation.weatherAPIResult
+      .pipe<any>(
+        map((data) => {
+          data.list.map((day: any) => {
+            day.main.temp = Math.round(day.main.temp);
+            day.main.temp_min = Math.round(day.main.temp_min);
+            day.main.temp_max = Math.round(day.main.temp_max);
+          });
+          data.list[0].main.feels_like = Math.round(
+            data.list[0].main.feels_like
+          );
+          data.city.sunrise = this.datePipe.transform(
+            new Date(data.city.sunrise * 1000),
+            'h:mm'
+          );
+          data.city.sunset = this.datePipe.transform(
+            new Date(data.city.sunset * 1000),
+            'h:mm'
+          );
+          return data;
+        })
+      )
+      .subscribe((res) => {
+        console.log(res);
+        this.userLocationWeather = res;
+      });
+  }
 }
--- src/app/widgets/location-city/location-city.component.ts
+++ src/app/widgets/location-city/location-city.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-location-city',
   templateUrl: './location-city.component.html',
-  styleUrls: ['./location-city.component.scss']
+  styleUrls: ['./location-city.component.scss'],
 })
 export class LocationCityComponent {
-
+  @Input('weather') weather: any;
 }
